a5582e6a73dd9001bae9c68ddedbd451a83f3649
JeromePoidevin <poidevin@free.fr>
2013-03-11 18:54:52 +0100

    add java/HelloTest.java

---
+++ java/HelloTest.java
@@ -0,0 +1,12 @@
+
+class HelloTest
+{
+    public static void main ( String [] argv )
+    {
+        System.out.println( "** HelloTest **" );
+        Hello h1 = new Hello("Test1") ;
+        Hello h2 = new Hello("Test2") ;
+        Hello h3 = new Hello("Test3") ;
+    }
+}
+
