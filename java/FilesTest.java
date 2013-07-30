291e86b915ccdc7e6138bf3861d295a46f8dd4f5
JeromePoidevin <poidevin@free.fr>
2013-07-31 00:06:50 +0200

    test Piped Writer+Reader

--- java/FilesTest.java
+++ java/FilesTest.java
@@ -11,11 +11,21 @@ class FilesTest
         {
             // java 6
             //Reader flux_fichier = new FileReader( "README" ) ;
+
+            // BufferedReader
             Reader flux_buf = new BufferedReader(flux_fichier) ;
-            int c;
-            for ( c=0 ; c!=-1 ; c=flux_buf.read() )
+
+            // PipedReader
+            PipedWriter pw = new PipedWriter();
+            PipedReader pr = new PipedReader(pw);
+
+            int c,d;
+            for ( c=0 ; c!=-1 ; c=flux_fichier.read() )
             {
+                pw.write(c) ;
+                d = pr.read() ;
                 System.out.print( (char)c ) ;
+                System.out.print( (char)d ) ;
             }
         }
         catch (IOException ex)
