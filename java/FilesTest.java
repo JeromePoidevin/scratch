fe10e0cee164052ea2d0d263bee721113110f409
JeromePoidevin <poidevin@free.fr>
2013-07-30 23:46:34 +0200

    java 7 : try new FileReader

--- java/FilesTest.java
+++ java/FilesTest.java
@@ -6,8 +6,11 @@ class FilesTest
 {
     public static void main ( String[] args )
     {
-        try {
-            Reader flux_fichier = new FileReader( "README" ) ;
+        // java 7
+        try ( Reader flux_fichier = new FileReader( "README" ) )
+        {
+            // java 6
+            //Reader flux_fichier = new FileReader( "README" ) ;
             Reader flux_buf = new BufferedReader(flux_fichier) ;
             int c;
             for ( c=0 ; c!=-1 ; c=flux_buf.read() )
