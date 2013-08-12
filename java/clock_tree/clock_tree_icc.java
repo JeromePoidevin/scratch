139a486ef0519ebdeb4c55596518690678b2b776
JeromePoidevin <poidevin@free.fr>
2013-08-12 23:30:08 +0200

    parser quite ok

--- java/clock_tree/clock_tree_icc.java
+++ java/clock_tree/clock_tree_icc.java
@@ -18,12 +18,15 @@ public class clock_tree_icc {
         CTSNode CTS = new CTSNode( filename , null ) ;
         String line = "" ;
         String clk = "" ;
-        Pattern pRoot = Pattern.compile( "Printing structure within exceptions of (.*) at root pin" ) ;
+        Matcher m ;
+        Pattern reRoot = Pattern.compile( "Printing structure within exceptions of (.*) at root pin" ) ;
+        Pattern reTreeStructure = Pattern.compile( " *.([0-9]+). (.*?)" ) ;
         ArrayList<CTSNode> hier = new ArrayList<>() ;
 
         while ( (line=readbuf.readLine()) != null )
         {
-            Matcher m = pRoot.matcher(line) ;
+            // clock declaration
+            m = reRoot.matcher(line) ;
             if (m.find())
             {
                 clk = m.group(1) ;
@@ -32,7 +35,26 @@ public class clock_tree_icc {
                 hier.add( CLK ) ;
                 continue ;
             }
+
             if (clk=="") continue ;
+
+            // line matches tree structure
+            m = reTreeStructure.matcher(line) ;
+            if (! m.find()) continue ;
+
+            int level = Integer.parseInt(m.group(1)) ;
+            String inst = m.group(2) ;
+
+            // test level and hier
+            if (level <= (hier.size()-1))  // step up in hier
+                for (int i=hier.size()-1 ;  i>level ; i--)
+                    hier.remove(i);
+            CTSNode UP = hier.get( hier.size()-1 ) ;
+            CTSNode CURRENT = new CTSNode( line , UP ) ;
+            hier.add( CURRENT ) ;
+
+            if (debug)
+                System.out.println( "\nUP = "+UP+"\nCURRENT = "+CURRENT );
         }
 
         return CTS ;
