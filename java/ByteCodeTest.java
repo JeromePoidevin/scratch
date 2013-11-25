55f916374bdfa10cbf4f4f6f9271e42785af3e00
JeromePoidevin <poidevin@free.fr>
2013-11-25 21:57:59 +0100

    play with int/float

--- java/ByteCodeTest.java
+++ java/ByteCodeTest.java
@@ -3,13 +3,22 @@ public class ByteCodeTest {
 
     public static void main(String args[]) {
 
-        int i,j ;
-        final int [] iconst = {1,2,3,4,5,6,7,8} ;
-        final float [] fconst = {1,2,3,4,5,6,7,8} ;
-        int [] itab = iconst ;
-        float [] ftab = fconst ;
+        int i = 5 ;
+        float f = (float) 5.0 ;
+        
+        int [] itab = new int[4] ;
+        float [] ftab = new float[4] ;
 
-        for (i=1;i<10;i++) { System.out.println(i) ; itab[i]*=3 ; ftab[i]*=5 ; }
+        itab[1] = 11 ;
+        ftab[2] = 12 ;
+
+        final int [] iconst = {4,3,2,1} ;
+        final float [] fconst = {8,7,6,5} ;
+
+        itab = iconst ;
+        ftab = fconst ;
+
+        for (i=1;i<4;i++) { itab[i]*=3 ; ftab[i]*=5 ; }
     }
 }
 
