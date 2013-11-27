dbc55d640887489b8a2e5b75bc41398f9818efd1
JeromePoidevin <poidevin@free.fr>
2013-11-27 06:03:53 +0100

    reorg int/float/String

--- java/ByteCodeTest.java
+++ java/ByteCodeTest.java
@@ -4,21 +4,24 @@ public class ByteCodeTest {
     public static void main(String args[]) {
 
         int i = 5 ;
-        float f = (float) 5.0 ;
-        
         int [] itab = new int[4] ;
-        float [] ftab = new float[4] ;
-
         itab[1] = 11 ;
-        ftab[2] = 12 ;
-
         final int [] iconst = {4,3,2,1} ;
-        final float [] fconst = {8,7,6,5} ;
-
         itab = iconst ;
-        ftab = fconst ;
+        for (i=1;i<4;i++) { itab[i]*=3 ; }
 
-        for (i=1;i<4;i++) { itab[i]*=3 ; ftab[i]*=5 ; }
+        float f = (float) 5.5 ;
+        float [] ftab = new float[4] ;
+        ftab[2] = (float) 12.12 ;
+        final float [] fconst = {8,7,6,5} ;
+        ftab = fconst ;
+        for (i=1;i<4;i++) { ftab[i]*=5 ; }
+
+        String s = "a" ;
+        String stab = "" ;
+        final String sconst = "hello" ;
+        stab = sconst ;
+
     }
 }
 
