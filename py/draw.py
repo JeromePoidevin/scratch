7a2fb3e2e0e6aafedb9373231a184d9da43cdef6
JeromePoidevin <poidevin@free.fr>
2015-11-15 00:25:48 +0100

    xy_def2win() transform coordinates ; draw(**arg) to pass drawing options ; initialize def reader (not tested)

--- py/draw.py
+++ py/draw.py
@@ -1,53 +1,134 @@
 from Tkinter import *
 
-class Cell :
+debug = True 
 
-    def __init__ (self,x,y) :
-        self.x = x
-        self.y = y
+########################################
+## (def coordinates) -> (win coordinates)
 
-    def draw (self) :
+def xy_def2win (dx,dy) :
+    global scale
+    global win_y
+    wx = (dx * scale)  # fixme : + fp_x1
+    wy = win_y - (dy * scale)  # fixme : + fp_y1
+    if debug : print "( %d , %d ) -> ( %d , %d )" % (dx,dy,wx,wy) 
+    return (wx,wy)
+
+
+########################################
+## classes for DEF objects
+
+class Inst :
+
+    def __init__ (self,name,x,y) :
+        self.name = name
+        ( self.x , self.y ) = ( x , y )
+        global debug
+        if debug : print self
+
+    def __str__ (self) :
+        return "Inst ( %d , %d ) %s" % (self.x,self.y,self.name)
+
+    def draw (self,**arg) :  # **arg : key=value options for create_rectangle
         global w
-        w.create_oval(self.x,self.y,50,50,fill="red")
+        ( x1 , y1 ) = xy_def2win( self.x , self.y )
+        ( x2 , y2 ) = ( x1+5 , y1-5 )  # y-5 because of inverted y-axis
+        w.create_rectangle(x1,y1,x2,y2,arg)
 
-class MacroCell :
 
-    def __init__ (self,x,y,sizex,sizey) :
-        self.y = y
-        self.sizey = sizey
-        self.x = x
-        self.sizex = sizex
-        self.centrex = x+0.5*sizex
-        self.centrey = y+0.5*sizey
+class MacroInst :
 
-    def draw (self) :
+    def __init__ (self,name,x,y,sizex,sizey) :
+        self.name = name
+        ( self.x1 , self.y1 ) = ( x , y )
+        ( self.x2 , self.y2 )  = ( x+sizex , y+sizey )
+        ( self.x  , self.y  ) = ( x+0.5*sizex , y+0.5*sizey )
+        global debug
+        if debug : print self
+
+    def __str__ (self) :
+        return "MacroInst ( %d , %d ) %s" % (self.x,self.y,self.name)
+
+    def draw (self,**arg) :   # **arg : key=value options for create_rectangle
         global w
-        global fpsize
-        w.create_rectangle(self.x,self.y,(self.sizex/fpsize),(self.sizey/fpsize))
+        ( x1 , y1 ) = xy_def2win( self.x1 , self.y1 )
+        ( x2 , y2 ) = xy_def2win( self.x2 , self.y2 )
+        w.create_rectangle(x1,y1,x2,y2,arg)
 
 
-master = Tk()
+########################################
+## read file
 
-w = Canvas(master, width=500, height=500)
-w.pack()
+def read_def( filename ):
 
-fpx = float(1200)
-fpy = float(900)
-fpsize = max(fpx,fpy)
-w.create_rectangle(0,0,(fpx/fpsize),(fpy/fpsize))
+    global debug
+    global scale
+    global floorplan
+    global instances
+    scale     = float()
+    floorplan = dict()
+    instances = dict()
 
-c1 = Cell(100,100)
-c2 = Cell(300,300)
-c1.draw()
-c2.draw()
-m1 = MacroCell(100,100,200,200)
-m2 = MacroCell(300,300,50,100)
-m1.draw()
-m2.draw()
+    section = ''
 
-mainloop()
+    print( "Info : read_def '%s'" % filename )
 
-#w.create_line(0, 0, 200, 100)
-#w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
+    F = open(filename,'r')
 
+    for line in F :
+
+        ## size
+        m = re.match('DIEAREA \( (\d+) (\d+) \) \( (\d+) (\d+) \)' ,line)
+        if m :
+            fp_x1 = m.group(1)
+            fp_y1 = m.group(2)
+            fp_x2 = m.group(3)
+            fp_y2 = m.group(4)
+            scale = min( win_x/(fp_x2-fp_x1) , win_y/(fp_y2-fp-y1) )
+            floorplan[''] = MacroInst('',fp_x1,fp_y1,fp_x2,fp_y2)
+            continue
+
+        ## instances
+        if re.match('COMPONENTS ',line) :
+            section = 'COMPONENTS'
+            continue
+        elif re.match('END COMPONENTS ',line) :
+            section = ''
+            return  ## only read COMPONENTS !
+
+        if section != 'COMPONENTS' :
+            continue
+
+
+########################################
+## standalone __main__
+
+if ( __name__ == "__main__" ) :
+
+    master = Tk()
+    
+    win_x = 500
+    win_y = 500
+    w = Canvas(master, width=win_x, height=win_y)
+    w.pack()
+    
+   #read_def( 'chip_floor.def' )
+
+    fp_x = float(1200)
+    fp_y = float(900)
+    scale = min( (win_x/fp_x) , (win_y/fp_y) )
+    # floorplan is a regular Rectangle with no fill
+    fp = MacroInst('',0,0,fp_x,fp_y)
+    fp.draw()
+    
+    MacroInst('m1',100,100,200,200).draw(fill='grey')
+    MacroInst('m2',300,300,200,200).draw(fill='blue')
+    Inst('i1',100,100).draw(fill='red')
+    Inst('i2',300,300).draw(fill='green')
+
+   
+    mainloop()
+    
+    #w.create_rectangle( (100,100),(300,300), fill='yellow' )
+    #w.create_line(0, 0, 200, 100)
+    #w.create_line(0, 100, 200, 0, fill="red", dash=(4, 4))
 
