bbba3f5bc709b6790ff061d03019653608933c97
JeromePoidevin <poidevin@free.fr>
2016-03-14 22:36:24 +0100

    vcd_file

--- SystemC/test_fifo.cpp
+++ SystemC/test_fifo.cpp
@@ -2,7 +2,16 @@
 
 #define FIFO_SIZE 16
 
+//----------------------------------------
+// module : test_fifo
+//----------------------------------------
+
 SC_MODULE(test_fifo) {
+
+  sc_out<int>  out_free;
+  sc_out<int>  out_available;
+  sc_out<bool> out_full ;
+  sc_out<bool> out_empty ;
 
   void test_fifo::fi(void);
   void test_fifo::fo(void);
@@ -47,14 +56,21 @@ void test_fifo::fo(void) {
     wait(3, SC_NS);
 
     std::cout << sc_time_stamp() << " : FO ";
-		if (u_fifo.nb_read(val)) { std::cout << val << std::endl; }
+    if (u_fifo.nb_read(val)) { std::cout << val << std::endl; }
     else                     { std::cout << "empty" << std::endl; }
   }
 }
 
 void test_fifo::stats(void) {
   for (;;) {
-    wait(10, SC_NS);
+    for (int i=0;i<10;i++)
+    {
+      wait(1, SC_NS);
+      out_free      = u_fifo.num_free();
+      out_available = u_fifo.num_available();
+      out_full      = (u_fifo.num_free()==0);
+      out_empty     = (u_fifo.num_available()==0);
+    }      
 
     std::cout << sc_time_stamp() << " :"
               << " avail " << u_fifo.num_available()
@@ -63,9 +79,34 @@ void test_fifo::stats(void) {
   }
 }
 
+//----------------------------------------
+// sc_main
+//----------------------------------------
+
 int sc_main(int argc, char* argv[]) {
-  test_fifo u_test_fifo ("TEST FIFO");
+
+  sc_signal<int>  out_free ;
+  sc_signal<int>  out_available ;
+  sc_signal<bool> out_full ;
+  sc_signal<bool> out_empty ;
+
+  test_fifo u_test_fifo( "u_test_fifo" ) ;
+  u_test_fifo.out_free     ( out_free );
+  u_test_fifo.out_available( out_available );
+  u_test_fifo.out_full     ( out_full );
+  u_test_fifo.out_empty    ( out_empty );
+
+  sc_trace_file * vcd_file;
+  vcd_file = sc_create_vcd_trace_file( "test_fifo" );
+  sc_trace( vcd_file , out_free      , "out_free" );
+  sc_trace( vcd_file , out_available , "out_available" );
+  sc_trace( vcd_file , out_full      , "out_full" );
+  sc_trace( vcd_file , out_empty     , "out_empty" );
+
   sc_start(1000, SC_NS);
+
+  sc_close_vcd_trace_file( vcd_file );
+
   return 0;
 }
 
